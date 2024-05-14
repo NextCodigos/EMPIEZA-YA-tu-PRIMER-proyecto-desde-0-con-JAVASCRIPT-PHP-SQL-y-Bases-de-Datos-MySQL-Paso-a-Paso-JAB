66e83708d1b9658b55e8dd09c2c3b512143e235d
NextCodigos <nebulanemesis376@gmail.com>
2024-05-14 23:29:46 +0200

    Añado filtros por busquedas

--- select copy 2.php
+++ select copy 2.php
@@ -7,16 +7,21 @@ $bd = "listado";
 
 // Estableciendo la conexión a la base de datos
 $conn = new mysqli($servidor, $usuario, $password, $bd);
-
-// Verificar si la conexión fue exitosa
 if ($conn->connect_error) {
-    // Si hay un error en la conexión, mostrar mensaje de error y terminar la ejecución
     die("Error de Conexión: " . $conn->connect_error);
 }
 
 // Consulta SQL para seleccionar todos los registros de la tabla "lenguajes"
-// y filtrar aquellos lenguajes que iniciaron antes del año 2000 y cuyo nombre
-// comienza con "Jav" y tiene una letra adicional (patrón "Jav_")
+// $sql = "SELECT id, nombre_de_lenguaje AS lenguaje, creador, inicio FROM lenguajes WHERE inicio<2000 ORDER BY inicio ASC";
+
+// $sql = "SELECT id, nombre_de_lenguaje AS lenguaje, creador, inicio FROM lenguajes WHERE inicio<2000 ORDER BY inicio DESC, lenguaje ASC LIMIT 4 OFFSET 1";
+
+// $sql = "SELECT id, nombre_de_lenguaje AS lenguaje, creador, inicio FROM lenguajes WHERE inicio<2000 AND nombre_de_lenguaje='Java' ORDER BY inicio DESC, lenguaje ASC ";
+
+// Con Java% que empiece por Java y los que sea
+// $sql = "SELECT id, nombre_de_lenguaje AS lenguaje, creador, inicio FROM lenguajes WHERE inicio<2000 AND nombre_de_lenguaje LIKE 'Java%' ORDER BY inicio DESC, lenguaje ASC ";
+
+// Con Jav_ que empiece por Jav y despues un caracter
 $sql = "SELECT id, nombre_de_lenguaje AS lenguaje, creador, inicio FROM lenguajes WHERE inicio<2000 AND nombre_de_lenguaje LIKE 'Jav_' ORDER BY inicio DESC, lenguaje ASC ";
 
 // Ejecutar la consulta
@@ -28,7 +33,6 @@ $lenguajes = array();
 // Verificar si hay resultados y almacenarlos en el array
 if ($resultado->num_rows > 0) {
     while ($fila = $resultado->fetch_assoc()) {
-        // Almacenar cada fila de resultados en el array de lenguajes
         $lenguajes[] = $fila;
     }
 }
