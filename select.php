3d1d6ee2528adc9cc23d2aa7e79cb989e0f3f7de
NextCodigos <nebulanemesis376@gmail.com>
2024-05-14 23:00:30 +0200

    Minuto 14:19 bien

--- select.php
+++ select.php
@@ -1,26 +1,37 @@
 <?php
-    $servidor="localhost";
-    $usuario="root";
-    $password="";
-    $bd="listado";
+// Configuración de la conexión a la base de datos
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$bd = "listado";
 
-    $conn = new mysqli($servidor, $usuario, $password, $bd);
-    if($conn->connect_error){
-        die("Error de Conexión: ".$conn->connect_error);
+// Estableciendo la conexión a la base de datos
+$conn = new mysqli($servidor, $usuario, $password, $bd);
+if ($conn->connect_error) {
+    die("Error de Conexión: " . $conn->connect_error);
+}
+
+// Consulta SQL para seleccionar todos los registros de la tabla "lenguajes"
+$sql = "SELECT * FROM lenguajes";
+
+// Ejecutar la consulta
+$resultado = $conn->query($sql);
+
+// Crear un array para almacenar los resultados de la consulta
+$lenguajes = array();
+
+// Verificar si hay resultados y almacenarlos en el array
+if ($resultado->num_rows > 0) {
+    while ($fila = $resultado->fetch_assoc()) {
+        $lenguajes[] = $fila;
     }
+}
 
-    $sql = "SELECT * FROM lenguajes";
-    $resultado=$conn->query($sql);
+// Recorrer el array de lenguajes y mostrar la información
+foreach ($lenguajes as $lenguaje) {
+    // Mostrar el nombre del lenguaje, su creador y año de inicio
+    echo $lenguaje["id"] . "º" . $lenguaje["nombre_de_lenguaje"] . " de " . $lenguaje["creador"] . "(" . $lenguaje["inicio"] . ")<br>";
+}
 
-    $lenguajes = array();
-    if($resultado->num_rows > 0){
-        while($fila = $resultado->fetch_assoc()){
-            $lenguajes[] = $fila;
-        }
-    }
-    foreach($lenguajes as $lenguaje){
-        echo $lenguaje["id"]. "º" . $lenguaje["nombre_de_lenguaje"] . " de " . $lenguaje["creador"] . "(" . $lenguaje["inicio"].")<br>";
-    }
-    $conn->close();
-
-?>
+// Cerrar la conexión a la base de datos
+$conn->close();
